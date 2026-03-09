627d29f1bde4be603003c96db7d84fcfabddd59d
bbaktaeho <bbaktaeho@gmail.com>
2026-03-09 22:12:48 +0900

    Reset codebase to docs-only state

--- mobile/android/app/src/main/kotlin/com/simsync/simsync_mobile/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.simsync.simsync_mobile
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- notecore/go.mod
+++
@@ -1,3 +0,0 @@
-module simsync/notecore
-
-go 1.26.1
